886c5f2a00a2de5e2b2c1e4c6ee99e14a2310e44
mubaraksakariya <mubaraksakariya@gmail.com>
2023-08-07 12:06:21 +0530

    before Hosting

--- Back end/Photosphere/urls.py
+++ Back end/Photosphere/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from Chat import consumers
 
 urlpatterns = [
     path("admin/", include("Admin.urls")),
@@ -11,7 +10,3 @@ urlpatterns = [
     path("story/", include("Story.urls")),
     path("chat/", include("Chat.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# websocket_urlpatterns = [
-#     path("ws/", consumers.MessageConsumer.as_asgi()),
-# ]
