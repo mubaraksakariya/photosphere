2575f99d43b993010b00bea209ba22a9a5514704
mubaraksakariya <mubaraksakariya@gmail.com>
2023-08-14 00:01:26 +0530

    url changes

--- Front end/src/Contexts/AxioContext.jsx
+++ Front end/src/Contexts/AxioContext.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://http://52.66.201.78/api/',
     headers: {
         'Content-Type': 'application/json',
     },
