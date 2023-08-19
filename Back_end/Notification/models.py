edd0d1d7fcc0da48cd067f507706cd34ab1532d5
mubaraksakariya <mubaraksakariya@gmail.com>
2023-08-19 16:51:54 +0530

    edited notifications

--- Back_end/Notification/models.py
+++ Back_end/Notification/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import serializers
 
 # Create your models here.
 
@@ -9,3 +10,13 @@ class Notification(models.Model):
     text = models.CharField(max_length=50)
     created_at = models.DateField(auto_now=False, auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+
+def serialize_notifiaction(notifiactio):
+    notifiactio_json = serializers.serialize("json", [notifiactio])
+    return notifiactio_json
+
+
+def serialize_notifiactions(users_queryset):
+    serialized_users = [serialize_notifiaction for user in users_queryset]
+    return serialized_users
--- Back_end/Notification/views.py
+++ Back_end/Notification/views.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse
+from .models import Notification, serialize_notifiaction, serialize_notifiactions
 
 # Create your views here.
 
 
 def getnotifications(request):
+    notifications = Notification.objects.all()
+    notifications = serialize_notifiactions(notifications)
     response_data = {
         "result": True,
+        "notifications": notifications,
     }
     return JsonResponse(response_data)
