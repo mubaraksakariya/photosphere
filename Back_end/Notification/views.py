97b4fa4c81dce1c71b25f91583e8225affa2a526
mubaraksakariya <mubaraksakariya@gmail.com>
2023-08-19 17:05:21 +0530

    Update views.py

--- Back_end/Notification/views.py
+++ Back_end/Notification/views.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
-from .models import Notification, serialize_notifiaction, serialize_notifiactions
+from .models import Notification, serialize_notification, serialize_notifications
 
 # Create your views here.
 
 
 def getnotifications(request):
     notifications = Notification.objects.all()
-    notifications = serialize_notifiactions(notifications)
+    notifications = serialize_notifications(notifications)
     response_data = {
         "result": True,
         "notifications": notifications,
